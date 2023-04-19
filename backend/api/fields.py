89b426024d34d59d16d0761a83bf250ae28d34aa
mac <mr.karlkarlsn@outlook.com>
2023-04-20 02:38:24 +0700

    Дерлой

--- backend/api/fields.py
+++ backend/api/fields.py
@@ -9,6 +9,13 @@ from django.core.files.base import ContentFile
 from rest_framework.serializers import ImageField
 
 
+def get_file_extension(file_name, decoded_file):
+
+    extension = imghdr.what(file_name, decoded_file)
+
+    return "jpg" if extension == "jpeg" else extension
+
+
 class Base64ImageField(ImageField):
 
     def to_internal_value(self, data):
@@ -22,16 +29,10 @@ class Base64ImageField(ImageField):
                 self.fail('invalid_image')
 
             file_name = str(uuid.uuid4())[:12]
-            file_extension = self.get_file_extension(file_name, decoded_file)
+            file_extension = get_file_extension(file_name, decoded_file)
 
             complete_file_name = "%s.%s" % (file_name, file_extension, )
 
             data = ContentFile(decoded_file, name=complete_file_name)
 
         return super(Base64ImageField, self).to_internal_value(data)
-
-    def get_file_extension(self, file_name, decoded_file):
-
-        extension = imghdr.what(file_name, decoded_file)
-
-        return "jpg" if extension == "jpeg" else extension

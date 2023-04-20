87b37fd7980eccf21fd9e5f748e6fba1434551ad
mac <mr.karlkarlsn@outlook.com>
2023-04-20 18:11:40 +0700

    Добавлена фильтрация

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,11 +76,17 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+    ],
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 6,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
     ]
 }
 # Password validation

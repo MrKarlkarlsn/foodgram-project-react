7828337b800faee62f5cbeb287ac8f6717cd12b2
mac <mr.karlkarlsn@outlook.com>
2023-04-26 20:19:36 +0700

    Исправления по фильтрам

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,5 +1,5 @@
 from django_filters.rest_framework import FilterSet, filters
-from recipe.models import Ingredient, Recipe
+from recipe.models import Ingredient, Recipe, Tag
 
 from users.models import CustomUsers
 

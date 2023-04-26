9edb9148723f8f952ba40045910c5b70847df973
mac <mr.karlkarlsn@outlook.com>
2023-04-27 00:50:32 +0700

    Админ панель

--- backend/recipe/admin.py
+++ backend/recipe/admin.py
@@ -3,7 +3,41 @@ from django.contrib import admin
 from .models import Ingredient, IngredientInRecipe, Recipe, Tag
 
 
-admin.site.register(Ingredient)
-admin.site.register(IngredientInRecipe)
-admin.site.register(Recipe)
-admin.site.register(Tag)
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'measurement_unit')
+    list_filter = ('name',)
+    search_fields = ('name',)
+    empty_value_display = '<пусто>'
+
+
+@admin.register(IngredientInRecipe)
+class IngredientInRecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'ingredient', 'recipe', 'quantity')
+    empty_value_display = '<пусто>'
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'pub_date', 'author', 'count_favorites',
+                    'count_tags', 'count_ingredients')
+    list_filter = ('author', 'tags', 'name')
+    search_fields = ('name',)
+    empty_value_display = '<пусто>'
+
+    @staticmethod
+    def count_favorites(obj):
+        return obj.favorites.count()
+
+    @staticmethod
+    def count_tags(obj):
+        return "\n".join([i[0] for i in obj.tags.values_list('name')])
+
+    @staticmethod
+    def count_ingredients(obj):
+        return "\n".join([i[0] for i in obj.ingredients.values_list('name')])
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug')
+    empty_value_display = '<пусто>'
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -3,5 +3,16 @@ from django.contrib import admin
 from .models import Subscribe, CustomUsers
 
 
-admin.site.register(CustomUsers)
-admin.site.register(Subscribe)
+@admin.register(CustomUsers)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'first_name', 'last_name')
+    list_filter = ('email', 'username')
+    search_fields = ('email', 'username')
+    empty_value_display = '<пусто>'
+
+@admin.register(Subscribe)
+class SubscribeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'user_author')
+    search_fields = ('user', 'user_author')
+    list_filter = ('user', 'user_author')
+    empty_value_display = '<пусто>'
